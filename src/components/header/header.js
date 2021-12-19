cbe42683726b45688cd6189328045151f39bdc48
ahmadhamzh <ahmadhamzh@ymail.com>
2021-12-19 23:55:46 +0200

    lab36

---
+++ src/components/header/header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './header.css'
+
+export default function Header() {
+    return (
+        <div id='headerDiv'>
+            <h1>OUR STORE</h1>
+            <nav>cart</nav>
+        </div>
+    )
+}
